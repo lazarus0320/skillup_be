835d32982e788075e4ffc6835e3d5b85eb568b05
lazarus0320 <astar5327z@gmail.com>
2024-08-12 09:59:04 +0900

    first commit

---
+++ src/main/java/org/example/sivillageclone/SiVillageCloneApplication.java
@@ -0,0 +1,13 @@
+package org.example.sivillageclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiVillageCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiVillageCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sivillageclone/SiVillageCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sivillageclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiVillageCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
